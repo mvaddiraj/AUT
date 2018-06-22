669397c6d8d0363eb123ca714ee9dc6b64cc6235
mvaddiraj <gourivad@gmail.com>
2018-06-22 10:16:18 +0100

    This is me

---
+++ Aut1/src/com/FirstProgram/MyFirstJavaProgram.java
@@ -0,0 +1,13 @@
+package com.FirstProgram;
+
+public class MyFirstJavaProgram {
+
+	public static void main(String[] args)
+	{
+		System.out.println("This is my first program");
+		System.out.println("To learn selenium");
+		System.out.println("To test a web application");
+ 
+	}
+
+}
